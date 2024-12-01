76021cd692f70e3bcbfb990e329299e8918f659a
LlamaLad7 <l3gomindstorms@gmail.com>
2024-12-01 14:07:24 +0000

    Day 1

---
+++ src/main/kotlin/day1/Day1.kt
@@ -0,0 +1,24 @@
+package day1
+
+import getInput
+import kotlin.math.abs
+
+fun main() {
+    day1part1(getInput(1, true, 1))
+    day1part1(getInput(1, false, 1))
+    day1part2(getInput(1, true, 2))
+    day1part2(getInput(1, false, 2))
+}
+
+private fun day1part1(lines: List<String>) {
+    val left = lines.map { line -> line.split("   ")[0].toInt() }
+    val right = lines.map { line -> line.split("   ")[1].toInt() }
+    println(left.sorted().zip(right.sorted()).sumOf { (a, b) -> abs(a - b) })
+}
+
+private fun day1part2(lines: List<String>) {
+    val left = lines.map { line -> line.split("   ")[0].toInt() }
+    val right = lines.map { line -> line.split("   ")[1].toInt() }.groupingBy { it }.eachCount()
+
+    println(left.sumOf { it * (right[it] ?: 0) })
+}
