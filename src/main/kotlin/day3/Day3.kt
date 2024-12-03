67c195e0315b3c2931f7d06c1698e4a2bbf3363c
LlamaLad7 <l3gomindstorms@gmail.com>
2024-12-03 09:27:57 +0000

    Day 3

---
+++ src/main/kotlin/day3/Day3.kt
@@ -0,0 +1,40 @@
+package day3
+
+import getInput
+
+fun main() {
+    day3part1(getInput(3, true, 1))
+    day3part1(getInput(3, false, 1))
+    day3part2(getInput(3, true, 2))
+    day3part2(getInput(3, false, 2))
+}
+
+private val mulRegex = "mul\\((\\d+),(\\d+)\\)".toRegex()
+
+private fun day3part1(lines: List<String>) {
+    println(
+        lines.flatMap {
+            mulRegex.findAll(it)
+        }
+            .sumOf {
+                it.groupValues[1].toInt() * it.groupValues[2].toInt()
+            }
+    )
+}
+
+private val commandRegex = "(do(n't)?\\(\\))|(mul\\((\\d+),(\\d+)\\))".toRegex()
+
+private fun day3part2(lines: List<String>) {
+    var enabled = true
+    var total = 0
+    for (match in lines.flatMap { commandRegex.findAll(it) }) {
+        val opcode = match.value.substringBefore('(')
+        when {
+            opcode == "do" -> enabled = true
+            !enabled -> continue
+            opcode == "don't" -> enabled = false
+            opcode == "mul" -> total += match.groupValues[4].toInt() * match.groupValues[5].toInt()
+        }
+    }
+    println(total)
+}
